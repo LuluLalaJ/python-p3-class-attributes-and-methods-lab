f63649561ecb68a5944fc208d2cee7a68296fb2a
Chen <chen.jiang.1206@gmail.com>
2023-05-04 15:30:21 -0400

    done - without using class methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,22 @@
 class Song:
-    pass
+    artists = []
+    genres = []
+    count = 0
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre) -> None:
+        Song.count += 1
+        self.artist = artist
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+            Song.artist_count[artist] = 1
+        elif artist in Song.artists:
+            Song.artist_count[artist] += 1
+        self.name = name
+        self.genre = genre
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+            Song.genre_count[genre] = 1
+        elif genre in Song.genres:
+            Song.genre_count[genre] += 1
